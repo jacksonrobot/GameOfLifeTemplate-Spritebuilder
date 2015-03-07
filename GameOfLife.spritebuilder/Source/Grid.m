e74a09a7d00baae56a363222aeb7f9c16c9c8708
金凯 <jinxiaolin@192.168.0.104>
2015-03-07 23:13:01 +0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -21,11 +21,44 @@ static const int GRID_COLUMNS =10;
     
     float _cellHeight;//放置生物的高
 }
--(void)onEnter{
+- (void)onEnter
+{
     [super onEnter];
+    
     [self setupGrid];
-    //accept touch on the grid
+    
+    // accept touches on the grid
     self.userInteractionEnabled = YES;
+}
+-(void)setupGrid
+{
+// divide the grid's size by the number of colums/rows to figure out the right width and height of each cell
+    _cellWidth = self.contentSize.width /GRID_COLUMNS;
+    _cellHeight = self.contentSize.height /GRID_ROWS;
+    
+    float x = 0;
+    float y = 0;
+    //initialize the array as a blank NSMutableArray
+    
+    _gridArray = [NSMutableArray array];
+    //initialize Creatures
+    for (int i = 0; i< GRID_ROWS; i++) {
+        _gridArray[i] = [NSMutableArray array];
+        x = 0;
+        
+        for (int j=0; j< GRID_COLUMNS; j++) {
+            Creature *creature = [[Creature alloc] initCreature];
+            creature.anchorPoint = ccp(0, 0);
+            creature.position = ccp(x, y);
+            [self addChild:creature];
+            //this is shorthand to access an array inisde an array
+            _gridArray[i][j] = creature;
+            //make creature visible to test this method,remove this once we know we have filled the grid peoperly
+            creature.isAlive = YES;
+            x += _cellWidth;
+        }
+        y +=_cellHeight;
+    }
 }
 
 @end
